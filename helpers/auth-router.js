5f4058cd7f00677f7e7e9fa269380e588cae3a51
fishmanjohn <john@emmaandjohnbyvan.com>
2020-01-29 16:08:20 -0800

    end points working now to token

---
+++ helpers/auth-router.js
@@ -0,0 +1,42 @@
+const bc = require("bcryptjs");
+const express = require("express")
+const router = express.Router()
+const Users = require('../helpers/users-model')
+
+router.post("/register", (req, res) => {
+    let user = req.body;
+
+    const hash = bc.hashSync(req.body.password, 3);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.post("/login", (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bc.compareSync(password, user.password)) {
+                res.status(200).json({ message: `Welcome ${user.username}!` });
+            } else {
+                res.status(401).json({ message: "Invalid Credentials" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+
+
+
+
+module.exports=router;
